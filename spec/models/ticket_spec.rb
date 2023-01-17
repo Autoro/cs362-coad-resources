6a5500e1dbdb240bcf7b820892d7d2989ac41909
Alex Miesbauer <alexdmiesbauer@gmail.com>
2023-01-17 11:59:41 -0800

    Updated ticket model spec to test for attribute existence.

--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Ticket, type: :model do
+    let(:ticket) { Ticket.new }
 
+    it "has a name" do
+        expect(ticket).to respond_to(:name)
+    end
+
+    it "has a description" do
+        expect(ticket).to respond_to(:description)
+    end
+
+    it "has a phone" do
+        expect(ticket).to respond_to(:phone)
+    end
+
+    it "has a closed" do
+        expect(ticket).to respond_to(:closed)
+    end
+
+    it "has a closed_at" do
+        expect(ticket).to respond_to(:closed_at)
+    end
 end
