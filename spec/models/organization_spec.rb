0ce4d49799c99ffd99827b4d62ae971396621a00
Alex Miesbauer <alexdmiesbauer@gmail.com>
2023-01-17 11:51:47 -0800

    Updated organization model spec to test for attribute existence.

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -1,7 +1,53 @@
 require 'rails_helper'
 
 RSpec.describe Organization, type: :model do
-    it "exists" do
-        Organization.new
+    let(:organization) { Organization.new }
+
+    it "has a name" do
+        expect(organization).to respond_to(:name)
+    end
+
+    it "has a status" do
+        expect(organization).to respond_to(:status)
+    end
+
+    it "has a phone" do
+        expect(organization).to respond_to(:phone)
+    end
+
+    it "has an email" do
+        expect(organization).to respond_to(:email)
+    end
+
+    it "has a description" do
+        expect(organization).to respond_to(:description)
+    end
+
+    it "has a rejection_reason" do
+        expect(organization).to respond_to(:rejection_reason)
+    end
+
+    it "has a liability_insurance" do
+        expect(organization).to respond_to(:liability_insurance)
+    end
+
+    it "has a primary_name" do
+        expect(organization).to respond_to(:primary_name)
+    end
+
+    it "has a secondary_name" do
+        expect(organization).to respond_to(:secondary_name)
+    end
+
+    it "has a secondary_phone" do
+        expect(organization).to respond_to(:secondary_phone)
+    end
+
+    it "has a title" do
+        expect(organization).to respond_to(:title)
+    end
+
+    it "has a transportation" do
+        expect(organization).to respond_to(:transportation)
     end
 end
