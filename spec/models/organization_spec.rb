4cd50f882d39a833fa7396e119cb2845ad0b9d7d
Alex Miesbauer <alexdmiesbauer@gmail.com>
2023-01-19 10:39:49 -0800

    Adding agreement attribute tests for organization model.

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -50,4 +50,36 @@ RSpec.describe Organization, type: :model do
     it "has a transportation" do
         expect(organization).to respond_to(:transportation)
     end
+
+    it "has an agreement_one" do
+        expect(organization).to respond_to(:agreement_one)
+    end
+
+    it "has an agreement_two" do
+        expect(organization).to respond_to(:agreement_two)
+    end
+
+    it "has an agreement_three" do
+        expect(organization).to respond_to(:agreement_three)
+    end
+
+    it "has an agreement_four" do
+        expect(organization).to respond_to(:agreement_four)
+    end
+
+    it "has an agreement_five" do
+        expect(organization).to respond_to(:agreement_five)
+    end
+
+    it "has an agreement_six" do
+        expect(organization).to respond_to(:agreement_six)
+    end
+
+    it "has an agreement_seven" do
+        expect(organization).to respond_to(:agreement_seven)
+    end
+
+    it "has an agreement_eight" do
+        expect(organization).to respond_to(:agreement_eight)
+    end
 end
