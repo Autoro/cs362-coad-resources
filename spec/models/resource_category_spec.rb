36a64699a10f8848a10b9a388f1b510ff5b97ba7
Alex Miesbauer <alexdmiesbauer@gmail.com>
2023-01-19 10:30:48 -0800

    Updated resource category model to test for attribute existence.

--- spec/models/resource_category_spec.rb
+++ spec/models/resource_category_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe ResourceCategory, type: :model do
+    let(:resource_category) { ResourceCategory.new }
 
+    it "has a name" do
+        expect(resource_category).to respond_to(:name)
+    end
+
+    it "has an active" do
+        expect(resource_category).to respond_to(:active)
+    end
 end
