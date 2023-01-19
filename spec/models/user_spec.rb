c720d37732b011c5e743b72d747620b80096dc00
Alex Miesbauer <alexdmiesbauer@gmail.com>
2023-01-19 10:31:12 -0800

    Updated user model to test for attribute existence.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-    it "exists" do
-        User.new
+    let(:user) { User.new }
+
+    it "has an email" do
+        expect(user).to respond_to(:email)
+    end
+
+    it "has a role" do
+        expect(user).to respond_to(:role)
     end
 end
