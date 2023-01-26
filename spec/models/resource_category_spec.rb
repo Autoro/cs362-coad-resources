e884785b2c480d4deed0d5ab9d922c11401454e5
Alex Miesbauer <alexdmiesbauer@gmail.com>
2023-01-26 11:30:17 -0800

    Added validation tests for resource category model.

--- spec/models/resource_category_spec.rb
+++ spec/models/resource_category_spec.rb
@@ -11,7 +11,13 @@ RSpec.describe ResourceCategory, type: :model do
         expect(resource_category).to respond_to(:active)
     end
 
-    it { should have_and_belong_to_many :organizations }
+    it { should have_and_belong_to_many(:organizations) }
 
-    it { should have_many :tickets }
+    it { should have_many(:tickets) }
+
+    it { should validate_presence_of(:name) }
+
+    it { should validate_length_of(:name).is_at_least(1).is_at_most(255).on(:create) }
+
+    it { should validate_uniqueness_of(:name).case_insensitive }
 end
