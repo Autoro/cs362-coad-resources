c477e039e37c4ed729cb08f0e1d76fdc2220f261
Alex Miesbauer <alexdmiesbauer@gmail.com>
2023-01-26 11:30:42 -0800

    Added validation tests for user model.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -11,5 +11,19 @@ RSpec.describe User, type: :model do
         expect(user).to respond_to(:role)
     end
     
-    it { should belong_to(:organization).optional } 
+    it { should belong_to(:organization).optional }
+
+    it { should validate_presence_of(:email) }
+
+    it { should validate_length_of(:email).is_at_least(1).is_at_most(255).on(:create) }
+
+    it { should allow_value("foo@bar.com").for(:email) }
+
+    it { should_not allow_value("asdf").for(:email) }
+
+    it { should validate_uniqueness_of(:email).case_insensitive }
+
+    it { should validate_presence_of(:password).on(:create) }
+
+    it { should validate_length_of(:password).is_at_least(7).is_at_most(255).on(:create) }
 end
