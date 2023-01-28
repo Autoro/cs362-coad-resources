b81370533a889f2a2bea3cb762ca03a9c997997b
Alex Miesbauer <alexdmiesbauer@gmail.com>
2023-01-27 17:46:56 -0800

    Added static function test for region model.

--- spec/models/region_spec.rb
+++ spec/models/region_spec.rb
@@ -7,6 +7,16 @@ RSpec.describe Region, type: :model do
     expect(region).to respond_to(:name)
   end
 
+  it "returns its name as a string" do
+    name = "Mt. Hood"
+    region.name = name
+    expect(region.to_s).to eq(name)
+  end
+
+  it "returns a Region with the name 'Unspecified'" do
+    expect(Region.unspecified.name).to eq("Unspecified")
+  end
+
   it { should have_many(:tickets) }
 
   it { should validate_presence_of(:name) }
@@ -14,11 +24,4 @@ RSpec.describe Region, type: :model do
   it { should validate_length_of(:name).is_at_least(1).is_at_most(255).on(:create) }
 
   it { should validate_uniqueness_of(:name).case_insensitive }
-
-  it "returns its name as a string" do
-    name = "Mt. Hood"
-    region.name = name
-    expect(region.to_s).to eq(name)
-  end
-
 end
