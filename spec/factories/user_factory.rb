c01ff7a63578a4f4ff8be4b7344c6e953f15e77b
Alex Miesbauer <alexdmiesbauer@gmail.com>
2023-02-21 11:48:14 -0800

    Added confirmed and admin traits to user factory.

--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -1,5 +1,14 @@
 FactoryBot.define do
     factory :user do
         email { "test@test.com" }
+        password { "password" }
+
+        trait :confirmed do
+            confirmed_at { Date.today }
+        end
+
+        trait :admin do
+            role { :admin }
+        end
     end
 end
