88790b53638c01a007d11a0214a1cc46df29cf4d
Alex Miesbauer <alexdmiesbauer@gmail.com>
2023-02-21 11:48:35 -0800

    Added dashboard controller tests.

--- spec/controllers/dashboard_controller_spec.rb
+++ spec/controllers/dashboard_controller_spec.rb
@@ -1,5 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe DashboardController, type: :controller do
+    context "as a non-user" do
+        it "GET index" do
+            get :index
+            expect(response).to redirect_to(new_user_session_path)
+        end
+    end
 
+    context "as a user" do
+        let(:user) { create(:user, :confirmed) }
+
+        before(:each) do
+            sign_in(user)
+        end
+    
+        it "GET index" do
+            get :index
+            expect(response).to be_successful
+        end
+    end
+
+    context "as an admin" do
+        let(:admin) { create(:user, :confirmed, :admin) }
+
+        before(:each) do
+            sign_in(admin)
+        end
+
+        it "GET index" do
+            get :index
+            expect(response).to be_successful
+        end
+    end
 end
