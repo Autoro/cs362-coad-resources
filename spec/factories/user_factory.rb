4428a31d5abe9e517770848d6b9d38fbd11f7250
Alex Miesbauer <alexdmiesbauer@gmail.com>
2023-03-02 11:18:30 -0800

    Added email sequence generator, and updated user factory.

--- spec/factories/user_factory.rb
+++ spec/factories/user_factory.rb
@@ -1,14 +1,15 @@
 FactoryBot.define do
     factory :user do
-        email { "test@test.com" }
+        role { :organization }
+        email { generate(:email) }
         password { "password" }
-
-        trait :confirmed do
-            confirmed_at { Date.today }
-        end
 
         trait :admin do
             role { :admin }
+        end
+
+        trait :confirmed do
+            confirmed_at { Date.today }
         end
     end
 end
