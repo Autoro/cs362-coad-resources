9e9c528ccc550865e04fed4bc3b060dac80e06c4
Alex Miesbauer <alexdmiesbauer@gmail.com>
2023-03-07 11:13:23 -0800

    Added ticket status tests to dashboard spec.

--- spec/controllers/dashboard_controller_spec.rb
+++ spec/controllers/dashboard_controller_spec.rb
@@ -19,10 +19,44 @@ RSpec.describe DashboardController, type: :controller do
             get :index
             expect(response).to be_successful
         end
+
+        it "GET index open tickets" do
+            get :index, params: { status: "Open" }
+            expect(response).to be_successful
+        end
+    end
+
+    context "as an approved user" do
+        let(:approved_organization) { create(:organization, :approved) }
+        let(:user) { create(:user, :confirmed, organization: approved_organization) }
+
+        before(:each) do
+            sign_in(user)
+        end
+
+        it "GET index" do
+            get :index
+            expect(response).to be_successful
+        end
+
+        it "GET index open tickets" do
+            get :index, params: { status: "Open" }
+            expect(response).to be_successful
+        end
+
+        it "GET index mycaptured tickets" do
+            get :index, params: { status: "My Captured" }
+            expect(response).to be_successful
+        end
+
+        it "GET index myclosed tickets" do
+            get :index, params: { status: "My Closed" }
+            expect(response).to be_successful
+        end
     end
 
     context "as an admin" do
-        let(:admin) { create(:user, :confirmed, :admin) }
+        let(:admin) { create(:user, :admin, :confirmed) }
 
         before(:each) do
             sign_in(admin)
@@ -30,6 +64,21 @@ RSpec.describe DashboardController, type: :controller do
 
         it "GET index" do
             get :index
+            expect(response).to be_successful
+        end
+
+        it "GET index open tickets" do
+            get :index, params: { status: "Open" }
+            expect(response).to be_successful
+        end
+
+        it "GET index captured tickets" do
+            get :index, params: { status: "Captured" }
+            expect(response).to be_successful
+        end
+
+        it "GET index closed tickets" do
+            get :index, params: { status: "Closed" }
             expect(response).to be_successful
         end
     end
